7ab69ab8fd17037b2980dd59c62f9c77182d2adf
calmdownkarm <karmanyaaggarwal@gmail.com>
2019-12-02 19:55:01 +0530

    updated preprocess for voter list extraction

--- preprocess.sh
+++ preprocess.sh
@@ -1,19 +1,16 @@
 #! /bin/bash
 
-if [ $# -lt 2 ]
-  then echo -e "Please provide an output directory and an input PDF. Example: pdf2hocr ./ocrd ~/Downloads/document.pdf"
-  exit 1
-fi
+for file in ../Voters/*.pdf; do
+  name=${file##*/}
+  base=${name%.pdf}
+  mkdir -p ../output2/$base
+done
 
-mkdir -p $1
-mkdir -p $1/png
-mkdir -p $1/tesseract
-mkdir -p $1/annotated
-mkdir -p $1/tables
+# gs -dBATCH -dNOPAUSE -sDEVICE=png16m -dGraphicsAlphaBits=4 -dTextAlphaBits=4 -r600 -sOutputFile="$1/png/page_%d.png" $2
 
-gs -dBATCH -dNOPAUSE -sDEVICE=png16m -dGraphicsAlphaBits=4 -dTextAlphaBits=4 -r600 -sOutputFile="$1/png/page_%d.png" $2
+# gs -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT -dMaxBitmap=900000000 -dAlignToPixels=0 -dGridFitTT=2 -sDEVICE=pnggray -r1200 -dDownScaleFactor=4 -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -dUseTrimBox -dQFactor=1 -dColorConversionStrategy=/Gray -dProcessColorModel=/DeviceGray -sOutputFile="$1/page_%d.png" $2
 
-cp $2 $1/orig.pdf
-pdftotext $1/orig.pdf - -enc UTF-8 > $1/text.txt
+# ls $1/png | grep -o '[0-9]\+' | parallel -j 4 "./process.sh $1 {}"
 
-ls $1/png | grep -o '[0-9]\+' | parallel -j 4 "./process.sh $1 {}"
+ls ../Voters/*.pdf | grep -o '[0-9]\+' | parallel -j 20 'gs -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT -dMaxBitmap=900000000 -dAlignToPixels=0 -dGridFitTT=2 -sDEVICE=pnggray -r1200 -dDownScaleFactor=4 -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -dUseTrimBox -dQFactor=1 -dColorConversionStrategy=/Gray -dProcessColorModel=/DeviceGray -sOutputFile="../output2/{}/page_%d.png" ../Voters/{}.pdf'
+# gs -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT -dMaxBitmap=900000000 -dAlignToPixels=0 -dGridFitTT=2 -sDEVICE=pnggray -r1200 -dDownScaleFactor=4 -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -dUseTrimBox -dQFactor=1 -dColorConversionStrategy=/Gray -dProcessColorModel=/DeviceGray -sOutputFile="$1/page_%d.png" {}
